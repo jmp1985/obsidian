dc1ea887d0ba7415248b753d015e89f006184527
Fiona Young <fiona.young.15@ucl.ac.uk>
2018-07-19 17:28:47 +0100

    the joiner program is for when an image folder has to be processed separately due to size

---
+++ obsidian/utils/joiner.py
@@ -0,0 +1,28 @@
+'''
+join two pickled datadicts together
+'''
+
+import pickle
+
+def pickle_get(path):
+  pickle_in = open(path, 'rb')
+  return pickle.load(pickle_in)
+
+def pickle_put(path, data):
+  pickle_out = open(path, 'wb')
+  pickle.dump(data, pickle_out)
+  pickle_out.close()
+
+def main():
+  
+  ID1 = 'T1g1'
+  ID2 = 'T1g1-2'
+
+  path = 'obsidian/datadump/{}_profiles.pickle'
+  part1 = pickle_get(path.format(ID1))
+  part1.update(pickle_get(path.format(ID2)))
+  
+  pickle_put(path.format(ID1), part1) 
+
+if __name__ == '__main__':
+  main()
